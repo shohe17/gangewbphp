893e7d67d89e4604f0696f71a87ff9f63ab36195
Atsushi <atsushi88885@gmail.com>
2020-08-02 01:08:24 +0900

    mdb導入

--- src/index.php
+++ src/index.php
@@ -18,7 +18,7 @@ require_once('Models/Post.php');
 $post = new Post();
 
 //$postsはgetall
-$posts = $post->getAll(); 
+$posts = $post->getAll();
 
 //オブジェクトにユーザー名を配置
 //オブジェクトにタイトル配置
@@ -28,32 +28,52 @@ $posts = $post->getAll();
 ?>
 <!DOCTYPE html>
 <html>
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Top | Gangサイト</title>
     <link rel="stylesheet" href="./assets/css/reset.css">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
+    <!-- Google Fonts -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap">
+    <!-- Bootstrap core CSS -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Material Design Bootstrap -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.19.1/css/mdb.min.css" rel="stylesheet">
     <link rel="stylesheet" href="./assets/css/style.css">
-    <script src="./assets/js/app.js" defer></script>
+
+    <!-- JQuery -->
+    <script defer type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <!-- Bootstrap tooltips -->
+    <script defer type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.4/umd/popper.min.js"></script>
+    <!-- Bootstrap core JavaScript -->
+    <script defer type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/js/bootstrap.min.js"></script>
+    <!-- MDB core JavaScript -->
+    <script defer type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.19.1/js/mdb.min.js"></script>
+    <script defer src="./assets/js/app.js"></script>
 </head>
+
 <body>
     <h1>トップページ</h1>
     <!-- タイトル表示 -->
     <!-- 画像表示 -->
     <div class="row p-3">
-           <!-- 先頭のデータから順に繰り返し -->
-           <?php foreach ($posts as $post) : ?>
+        <!-- 先頭のデータから順に繰り返し -->
+        <?php foreach ($posts as $post) : ?>
             <div class="col-sm-6 col-md-4 col-lg-3 py-3 py-3">
                 <div class="card">
                     <!-- dbにはいってるパスを指定したい -->
-                    <img src="<?php echo $post['image_at'];?>" class="card-img-top" alt="...">
+                    <img src="<?php echo $post['image_at']; ?>" class="card-img-top" alt="...">
                     <div class="card-body">
-                        <h5 class="card-title"><?php echo $post['title'];?></h5>
+                        <h5 class="card-title"><?php echo $post['title']; ?></h5>
                     </div>
                 </div>
             </div>
-          <?php endforeach;?>
-        </div>
+        <?php endforeach; ?>
+    </div>
 
 </body>
+
 </html>
